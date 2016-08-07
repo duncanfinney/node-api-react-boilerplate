590f00c366607cb81f775456a2253d97a9408f75
Duncan Finney <duncan.finney@gmail.com>
2016-08-07 14:28:45 -0400

    Get react-router stubbed out

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,3 +1,7 @@
 // Set up your root reducer here...
- import { combineReducers } from 'redux';
- export default combineReducers;
+import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
+
+export default combineReducers({
+  routing: routerReducer
+})
---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+const App = () => (
+  <h1>hello world</h1>
+)
+
+export default (
+  <Route path="/" component={App} />
+);
